b4507ab2261d4c645a56e5a6c5ce1b8bbf8cac9c
anthonymiloradovic <anthony.miloradovic@icloud.com>
2023-03-14 11:39:17 +0100

    routes and home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+
+import React from 'react';
+import IndexRoutes from './routes/IndexRoutes';
+
+
 function App() {
-  
+  return (
+    <div>
+      <IndexRoutes />
+    </div>
+  );
 }
 
-export default App
+export default App;
