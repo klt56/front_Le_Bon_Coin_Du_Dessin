28105e7ee5f3d7cbfff436fb504f55716da9b938
sarah <sarahmelaikia@gmail.com>
2023-03-21 09:58:00 +0100

    paralax-part

---
+++ src/components/Paralax3.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Parallax } from 'react-parallax';
+import '../styles/_parallax.scss';
+import Cafe from './img/cafe.jpg'
+
+const Paralax3 = () => (
+    <div className='parallax-container'>
+    <Parallax className='image' blur={0} bgImage={Cafe} strength={300} bgImageStyle={{minHeight:"100vh"}}>
+        <div className='content'>
+            <span className="img-txt">Sur tout le site</span>
+        </div>
+    </Parallax>
+    </div>
+);
+
+export default Paralax3;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,24 @@
 import Footer from '../components/Footer'
 import Tinder from '../components/Card/TinderCard';
-import Parallax from '../components/Parallax';
+import Paralax from '../components/Paralax';
+import Paralax2 from '../components/Paralax2';
+import Paralax3 from '../components/Paralax3';
+import Text from '../components/Text';
+import Text2 from '../components/Text2';
+import Text3 from '../components/Text3';
+
 
 const Home = () => {
     return (
-        <div>
-            
-            <Parallax />
-            <Tinder />
-            <Footer/>
+    <div>
+    <Paralax />
+    <Text/>
+    <Paralax2 />
+    <Text2/>
+    <Paralax3 />
+    <Text3/>
+    <Tinder />
+    <Footer/>
         </div>
     );
 };
